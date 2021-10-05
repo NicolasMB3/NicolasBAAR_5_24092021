db7d4380ad677a1ee7eb6894b2ab37b146469f1b
NicolasMB3 <nicolasbaar03@gmail.com>
2021-10-05 21:50:44 +0200

    Update panier :+1:

--- front/js/product.js
+++ front/js/product.js
@@ -1,18 +1,26 @@
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
 let url = 'http://localhost:3000/api/products/' + id;
+const numberSelect = document.getElementById('quantity');
+const itemImg = document.querySelector(".item__img");
+const itemTitle = document.getElementById("title");
+const itemPrice = document.getElementById("price");
+const itemDesc = document.getElementById("description");
 
-const fetchSofas = async() => {
+const fetchProduct = async() => {
    product = await fetch(url)
       .then(response => response.json())
       .then(function(resultatAPI) {
          article = resultatAPI;
          let min = 0, max = article.colors.length, select = document.getElementById('colors');
-         document.querySelector(".item__img").innerHTML = `<img src="${article.imageUrl}" alt="Photographie d'un canapé"></img>`;
-         document.getElementById("title").innerHTML = article.name;
-         document.getElementById("price").innerHTML = article.price;
-         document.getElementById("description").innerHTML = article.description;
-         for (var i = min; i<max; i++) {
+         // Placer les élèments si le fetch arrive à trouver les informations back-end
+         itemImg.innerHTML = `<img src="${article.imageUrl}" alt="Photographie d'un canapé"></img>`;
+         itemTitle.innerHTML = article.name;
+         itemPrice.innerHTML = article.price;
+         itemDesc.innerHTML = article.description;
+         document.title = 'Kanap - ' + article.name;
+         // Boucle pour afficher les options de couleurs dans le menu déroulant
+         for (var i = min; i < max; i++) {
             var opt = document.createElement('option');
             opt.value = i;
             opt.innerHTML = article.colors[i];
@@ -22,4 +30,13 @@ const fetchSofas = async() => {
       .catch(err => console.log('Erreur : ' + err));
 }
 
-fetchSofas();
+// Ajout au panier avec le localStorage
+function addPanier () {
+   const addButton = document.querySelector("#addToCart");
+   addButton.addEventListener('click', () => {
+      /// LocalStorage
+   });
+}
+
+fetchProduct();
+addPanier();
