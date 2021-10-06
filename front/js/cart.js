369f816f0482ed26efc33838ccb4c16a70e2c6a8
NicolasMB3 <nicolasbaar03@gmail.com>
2021-10-06 09:56:38 +0200

    Boom :angel:

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,12 +1,9 @@
 let itemSelect = JSON.parse(localStorage.getItem("products"));
 
-
 function displayCart() {
-   let test = document.querySelector("#cart__items");
- 
-   // Pour chaque objet dans le tableau copié du localStorage, on crée les divs de l'affichage du panier et on les remplit avec les données du tableau.
+   let cartItems = document.querySelector("#cart__items");
    for (let produit in itemSelect) { 
-      document.querySelector("#cart__items").innerHTML += 
+      cartItems.innerHTML += 
       `  <article class="cart__item" data-id="${itemSelect[produit].id}">
             <div class="cart__item__img">
                <img src="" alt="Photographie d'un canapé">
